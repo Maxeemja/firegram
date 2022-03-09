47a91e3a843e18bcb7dd083b96252fe668a7b22b
MXMJ <maxeemja.inc@gmail.com>
2022-03-09 23:11:55 +0200

    Updates

---
+++ precache-manifest.932aaa4104b6df64641fd4c80e56da5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca160e7bc8142d08b6e4dd949d6c3349",
+    "url": "/firegram/index.html"
+  },
+  {
+    "revision": "ee628c063017201a9340",
+    "url": "/firegram/static/css/main.598d1565.chunk.css"
+  },
+  {
+    "revision": "d3ee5fdd059d9455948d",
+    "url": "/firegram/static/js/2.dbaeea17.chunk.js"
+  },
+  {
+    "revision": "abd993ccc6cc85d4d89cbd0b44a03558",
+    "url": "/firegram/static/js/2.dbaeea17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee628c063017201a9340",
+    "url": "/firegram/static/js/main.402160df.chunk.js"
+  },
+  {
+    "revision": "a20b20e63cdcd0ca9e81",
+    "url": "/firegram/static/js/runtime-main.9968160c.js"
+  }
+]);
